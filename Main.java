d2385986801feddb534be0794801ce3031e152f5
MersChanachon <Chanachon.mers@gmail.com>
2025-04-11 12:52:04 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,16 +6,16 @@ int[] nums = {4,2,9,1,5};
         System.out.println(nums[i]);
 }
 public static void doSort(int[] arr){
-int a=0;
-while(a<arr.length-1){
-for(int b=0; b<arr.length-1; b++){
-if(arr[b]>arr[b+1]){
-int temp = arr[b];
-arr[b]=arr[b+1];
-arr[b+1]=temp;
+int i=0;
+while(i<arr.length-1){
+for(int j=0; j<arr.length-1; j++){
+if(arr[j]>arr[j+1]){
+int temp = arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
 }
 }
-a++;
+i++;
 }
 }
 }
